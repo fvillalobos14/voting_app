46c36a8a63230b55f9d68af3b48fa232d418b231
Francis Villalobos <Francis@Franciss-MacBook-Pro.local>
2018-03-10 17:06:23 -0600

    Cambiando el UpVote

--- public/js/app.js
+++ public/js/app.js
@@ -10,14 +10,22 @@ class ProductList extends React.Component {
   }
 
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
-}
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+  }
 
   render() {
     const products = this.state.products.sort((a,b) => (
       b.votes - a.votes
     ));
-    const productComponents = Seed.products.map((product) => (
+    const productComponents = products.map((product) => (
       <Product
       key={'product-' + product.id}
       id={product.id}
